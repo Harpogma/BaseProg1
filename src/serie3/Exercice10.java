67da093a88dbf856bf6a5179c9619bc2ec146312
Harpogma <gd.temp@pm.me>
2024-10-10 10:14:18 +0200

    Exercice10 is done

--- src/serie3/Exercice10.java
+++ src/serie3/Exercice10.java
@@ -10,6 +10,7 @@ public class Exercice10 {
         float plusHauteNote = 0f;
         float totalNotes = 0f;
         byte nombreNotesTotal = 0;
+        float moyenne = 0;
 
         do {
             Scanner scanner = new Scanner(System.in);
@@ -17,11 +18,10 @@ public class Exercice10 {
             note = scanner.nextFloat();
             scanner.nextLine();
 
-            if (note < 1 || note > 6) {
+            if (note > 1 && note < 6) {
                 if (note > plusHauteNote) {
                     plusHauteNote = note;
                 }
-
                 totalNotes += note;
                 nombreNotesTotal++;
             } else {
@@ -30,8 +30,13 @@ public class Exercice10 {
 
         } while (note >= 1 && note <= 6);
 
-        float moyenne = totalNotes / nombreNotesTotal;
+        if (nombreNotesTotal == 0) {
+            moyenne = 0;
+        } else {
+            moyenne = totalNotes / nombreNotesTotal;
 
-        System.out.println("La moyenne des " + (nombreNotesTotal - 1) + " notes est : " + String.format("%.2f ", moyenne));
+        }
+
+        System.out.println("La moyenne des " + nombreNotesTotal + " notes est : " + String.format("%.2f ", moyenne));
     }
 }
