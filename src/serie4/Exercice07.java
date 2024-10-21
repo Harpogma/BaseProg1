cbc128eeee99e9ff377e4dffce8d661c9040b55d
Harpogma <gd.temp@pm.me>
2024-10-21 18:12:23 +0200

    Exercice07 is done and works fine

--- src/serie4/Exercice07.java
+++ src/serie4/Exercice07.java
@@ -5,24 +5,13 @@ import java.util.Scanner;
 
 public class Exercice07 {
     public static void main(String[] args) {
-        ArrayList<String> myList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Veuillez saisir une phrase puis appuyez la touche entrer");
+        String input = scanner.nextLine();
 
-        byte compteur = 0;
-        String input;
-        while (true) {
-            input = scanner.nextLine();
-            if (input.isEmpty()) {
-                break;
-            }
-            myList.add(input);
+        String[] arrayOfStrings = input.split(" ");
+        for (int i = 0; i < arrayOfStrings.length; i++) {
+            System.out.print((i == 0 ? "[" : "") + "\"" + arrayOfStrings[i] + "\"" + (i == arrayOfStrings.length - 1 ? "]" : ","));
         }
-        scanner.close();
-
-        for (int i = 0; i < myList.size(); i++) {
-            System.out.print((i == 0 ? "[" : "") + "\"" + myList.get(i) + "\"" + (i == myList.size() - 1 ? "]" : ","));
-        }
-
     }
 }
