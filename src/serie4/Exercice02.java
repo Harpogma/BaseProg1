6f55c839c3414af104e6cfdb22ba53d944fa66e8
Harpogma <gd.temp@pm.me>
2024-10-31 12:55:07 +0100

    Some minor refactor

--- src/serie4/Exercice02.java
+++ src/serie4/Exercice02.java
@@ -12,6 +12,7 @@ public class Exercice02 {
             j--;
         }
 
+        System.out.println("Version 1");
         for (int i = 0; i < arrayToReverse.length; i++) {
             System.out.print(
                     (i == 0 ? "Avant : [" : "") + arrayToReverse[i] + (i == arrayToReverse.length - 1 ? "]" : ","));
@@ -21,6 +22,28 @@ public class Exercice02 {
 
         for (int i = 0; i < arrayReversed.length; i++) {
             System.out.print((i == 0 ? "Après : [" : "") + arrayReversed[i] + (i == arrayReversed.length - 1 ? "]" : ","));
+        }
+
+        //version plus optimisée car pas de 2e array de créé pour rien, donc moins de mémoire nécessaire
+        System.out.println();
+        System.out.println("Version 2");
+        int[] arrayToReverse2 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        for (int i = 0; i < arrayToReverse2.length; i++) {
+            System.out.print(
+                    (i == 0 ? "Avant : [" : "") + arrayToReverse[i] + (i == arrayToReverse.length - 1 ? "]" : ","));
+        }
+
+        for (int i = 0; i < arrayReversed.length/2; i++) {
+            int temp = arrayToReverse2[arrayToReverse2.length - (1+i)];
+            arrayToReverse2[arrayToReverse2.length-(1+i)] = arrayToReverse2[i];
+            arrayToReverse2[i] = temp;
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < arrayToReverse2.length; i++) {
+            System.out.print((i == 0 ? "Après : [" : "") + arrayToReverse2[i] + (i == arrayToReverse2.length - 1 ? "]" : ","));
         }
     }
 }
