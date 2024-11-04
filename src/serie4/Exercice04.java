bc4e849af8fce37a612eb80dd50c834c728f0b0d
Harpogma <gd.temp@pm.me>
2024-11-04 23:12:39 +0100

    Did the exercise in a more optimised way

--- src/serie4/Exercice04.java
+++ src/serie4/Exercice04.java
@@ -3,26 +3,23 @@ package serie4;
 public class Exercice04 {
     public static void main(String[] args) {
         String[] arrayToBeShift = {"Aline","Roger","Julia","Steven","Jules"};
-        String[] shiftedArray = new String[arrayToBeShift.length];
-        byte shifter = 0;
-
-        for (int i = 0; i < arrayToBeShift.length; i++) {
-            if (i == arrayToBeShift.length - 1) {
-                shiftedArray[shifter] = arrayToBeShift[0];
-            } else {
-                shiftedArray[shifter] = arrayToBeShift[i + 1];
-                shifter++;
-            }
-        }
 
         for (int i = 0; i < arrayToBeShift.length; i++) {
             System.out.print((i == 0 ? "Avant : [\"" : "\"") + arrayToBeShift[i] + "\"" + (i == arrayToBeShift.length - 1 ? "]" : ","));
         }
 
-        System.out.println();
+        String temp  = arrayToBeShift[0];
+        for (int i = 0; i < arrayToBeShift.length; i++) {
+            if (i == arrayToBeShift.length - 1) {
+                arrayToBeShift[arrayToBeShift.length - 1] = temp;
+            } else {
+                arrayToBeShift[i] = arrayToBeShift[i + 1];
+            }
+        }
 
-        for (int i = 0; i < shiftedArray.length; i++) {
-            System.out.print((i == 0 ? "Après : [\"" : "\"") + shiftedArray[i] + "\"" + (i == arrayToBeShift.length - 1 ? "]" : ","));
+        System.out.println();
+        for (int i = 0; i < arrayToBeShift.length; i++) {
+            System.out.print((i == 0 ? "Après : [\"" : "\"") + arrayToBeShift[i] + "\"" + (i == arrayToBeShift.length - 1 ? "]" : ","));
         }
     }
 }
