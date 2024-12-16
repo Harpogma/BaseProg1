950237eb19a861809cad483d84fac0d358c734a1
Harpogma <gd.temp@pm.me>
2024-12-16 11:50:42 +0100

    Exercice02 is done

--- src/serie6/Exercice02.java
+++ src/serie6/Exercice02.java
@@ -2,6 +2,8 @@ package serie6;
 
 public class Exercice02 {
     public static void main(String[] args) {
-
+        //to calculate the max number of iteration to find the value in an array with
+        // the binary search is log2(tab.length)
+        //the answer of the exercise is log2(300000) = 18.2 (approx)
     }
 }
