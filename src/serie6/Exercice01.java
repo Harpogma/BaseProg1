6f90fe216ba74092b6fd77786c9c0a5870a9be75
Harpogma <gd.temp@pm.me>
2024-12-16 08:36:51 +0100

    Done the function with a do/while instead of a for

--- src/serie6/Exercice01.java
+++ src/serie6/Exercice01.java
@@ -1,4 +1,4 @@
-package serie7_Robin;
+package serie6;
 
 public class Exercice01 {
 
@@ -8,48 +8,32 @@ public class Exercice01 {
         int indexSup = tab.length - 1;
         int indexRech = (indexInf + indexSup) / 2;
 
-//        for (int i = result; i <= indexSup; i++) {
-//            if (number == tab[indexRech]) {
-//                result = indexRech;
-//            } else {
-//                if (number < tab[indexRech]) {
-//                    indexSup = indexRech;
-//                    indexRech = (indexInf + indexSup) / 2;
-//                } else if (number > tab[indexRech] && number < tab[tab.length - 1]) {
-//                    indexInf = indexRech;
-//                    indexRech = (indexInf + indexSup) / 2;
-//                } else {
-//                    result = -1;
-//                }
-//            }
-//        }
-
         do {
             if (number == tab[indexRech]) {
                 result = indexRech;
             } else {
-                if (number < tab[indexRech]) {
+                if (number < tab[indexRech] && number > tab[indexRech + 1]) {
                     indexSup = indexRech;
                     indexRech = (indexInf + indexSup) / 2;
+                    result = indexRech;
                 } else if (number > tab[indexRech] && number < tab[tab.length - 1]) {
                     indexInf = indexRech;
                     indexRech = (indexInf + indexSup) / 2;
+                    result = indexRech;
                 } else {
                     result = -1;
                 }
             }
-        } while (number != result && indexInf + 1 != indexSup);
+        } while (number != tab[indexRech] && result != -1);
 
         return result;
     }
 
     public static void main(String[] args) {
         int[] tab = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int numberToSearch = 7;
+        int numberToSearch = 6;
 
         int isNumberIn = indexOf(tab, numberToSearch);
         System.out.println(isNumberIn);
-
-
     }
 }
