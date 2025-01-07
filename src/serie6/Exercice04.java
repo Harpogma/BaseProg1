42c8d7b9dfa0b2df9c6137ce206e342b488fb469
Harpogma <gd.temp@pm.me>
2025-01-07 07:43:36 +0100

    Implementing function to sort the array

--- src/serie6/Exercice04.java
+++ src/serie6/Exercice04.java
@@ -4,20 +4,59 @@ import java.util.Scanner;
 
 public class Exercice04 {
 
-    public static void insertValues(int number) {
+    private static void leftShift(int[] tab, int index) {
+       for (int i = index; i > 0; i--) {
+           tab[i] = tab[i - 1];
+       }
+    }
+
+    public static void insertValues(int[] tab, int number, int index) {
+
+        boolean numberPlaced = false;
+
+        for (int i = index - 1; i < index; i++) {
+            while (!numberPlaced) {
+                if (number < tab[i]) {
+                    leftShift(tab, index);
+                    tab[i] = number;
+                    numberPlaced = true;
+                } else if (number > tab[i] && tab[i + 1] == 0) {
+                    tab[i + 1] = number;
+                    numberPlaced = true;
+                } else if (number == tab[i]) {
+                    leftShift(tab, index);
+                    tab[i] = number;
+                    numberPlaced = true;
+                }
+            }
+        }
 
     }
+
+    public static void printArray(int[] tab, int size) {
+        for (int number : tab) {
+            System.out.print(number + " ");
+        }
+    }
+
     public static void main(String[] args) {
 
-        final int size = 20;
+        final int size = 4;
+        int[] values = new int[size];
 
         for (int i = 0; i < size; i++) {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Insertion de : ");
             int number = scanner.nextInt();
 
-            insertValues(number);
+            if (i == 0) {
+                values[0] = number;
+            } else {
+                insertValues(values, number, i);
+            }
         }
+
+        printArray(values, size);
 
     }
 }
