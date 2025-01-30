4f2858cc21508b26a6559c542b64303f7cef4035
Harpogma <gd.temp@pm.me>
2025-01-30 16:10:14 +0100

    Exercise to practice binary search and sort

---
+++ src/anciensTests/testBlanc.java
@@ -0,0 +1,4 @@
+package anciensTests;
+
+public class testBlanc {
+}
--- src/serie6/Exercice04.java
+++ src/serie6/Exercice04.java
@@ -1,85 +1,55 @@
 package serie6;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercice04 {
 
-    private static void leftShift(int[] tab,int index, int lastNumberIndex) {
-
-
-        for (int i = lastNumberIndex; i > index; i--) {
-            if (i > index) {
-                int temp = tab[i + 1];
-                tab[i + 1] = tab[i];
-                tab[i] = temp;
-            } else {
-                tab[i + 1] = tab[i];
-            }
-       }
+    public static int indexOfAge (int[] ages, int ageToFind) {
+        return Arrays.binarySearch(ages, ageToFind);
     }
 
-    private static int lastNumberIndex(int[] tab, int index) {
-        int lastNumberIndex = 0;
-        for (int i = 1; i <= index; i++) {
-            if (tab[i] == 0) {
-                lastNumberIndex = i - 1;
-            } else {
-                System.out.println("The end of the array is reached!");
-            }
-        }
-
-        return lastNumberIndex;
-    }
-
-    public static void insertValues(int[] tab, int number, int index) {
-
-        boolean numberPlaced = false;
-
-        for (int i = index - 1; i < index; i++) {
-            while (!numberPlaced) {
-                int lastNumberIndex = lastNumberIndex(tab, index);
-                if (number < tab[i]) {
-                    System.out.println(lastNumberIndex);
-                    leftShift(tab, index, lastNumberIndex);
-                    tab[i] = number;
-                    numberPlaced = true;
-                } else if (number > tab[i] && tab[i + 1] == 0) {
-                    tab[i + 1] = number;
-                    numberPlaced = true;
-                } else if (number == tab[i]) {
-                    leftShift(tab, index, lastNumberIndex);
-                    tab[i] = number;
-                    numberPlaced = true;
-                }
-            }
-        }
-
-    }
-
-    public static void printArray(int[] tab, int size) {
-        for (int number : tab) {
-            System.out.print(number + " ");
+    public static void swap(int[] ages, int binarySearchResult, int lastIndex) {
+        for (int i = lastIndex; i >= binarySearchResult; i--) {
+            ages[i + 1] = ages[i];
         }
     }
 
     public static void main(String[] args) {
+        String[] names = {"Paul", "Simone", "Louis", "Anaïs"};
+        int[] ages = new int[10];
 
-        final int size = 4;
-        int[] values = new int[size];
-
-        for (int i = 0; i < size; i++) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Insertion de : ");
-            int number = scanner.nextInt();
-
-            if (i == 0) {
-                values[0] = number;
-            } else {
-                insertValues(values, number, i);
-            }
+        for (int i = 0; i < 5; i++) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Entrez un âge : ");
+            ages[i] = sc.nextInt();
+            sc.nextLine();
         }
 
-        printArray(values, size);
 
+        for(int age: ages) {
+            System.out.println(age);
+        }
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Quel âge veux-tu chercher ?");
+        int age = sc.nextInt();
+
+        int indexOfAge = indexOfAge(ages, age);
+        System.out.println(indexOfAge);
+        int indexNumberToInsert = Math.abs(indexOfAge) - 1;
+
+        int lastIndex = 4;
+
+        if(indexOfAge < 0) {
+            swap(ages, indexNumberToInsert, lastIndex);
+            ages[indexNumberToInsert] = age;
+        } else {
+            System.out.println("L'âge que tu cherches est à l'index : " + indexOfAge);
+        }
+
+        for(int j: ages) {
+            System.out.println(j);
+        }
     }
 }
